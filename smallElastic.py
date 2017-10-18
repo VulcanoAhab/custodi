5cc4167d4fdcb75b26b132c480c89781f1a2a087
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-10-18 21:54:10 -0200

    Initial Elastic improvement

--- smallElastic.py
+++ smallElastic.py
@@ -41,25 +41,25 @@ class Basics:
             index=cls._index
         if not docType:
             docType=cls._docType
-        self._conn.index(index=index,doc_type=docType,
+        cls._conn.index(index=index,doc_type=docType,
                         id=doc_id,body=doc)
     @classmethod
     def search(cls, query, index=None, docType=None):
         """
         """
-        return self._conn.search(index=index, doc_type=docType, body=query)
+        return cls._conn.search(index=index, doc_type=docType, body=query)
 
     @classmethod
     def delete(cls, doc_id, index=None, docType=None):
         """
         """
-        return self._conn.delete(index=index, doc_type=docType, id=doc_id)
+        return cls._conn.delete(index=index, doc_type=docType, id=doc_id)
 
     @classmethod
     def deleteByMatch(cls, macthQuery, index=None, docType=None):
         """
         """
-        return self._conn.delete_by_query(index=index,
+        return cls._conn.delete_by_query(index=index,
                                           doc_type=docType,
                                           body={'query':{"match":macthQuery}})
 
@@ -67,20 +67,20 @@ class Basics:
     def deleteAllbyIndex(cls, index=None, docType=None):
         """
         """
-        return self._conn.delete_by_query(index=index, doc_type=docType,
+        return cls._conn.delete_by_query(index=index, doc_type=docType,
                                           body={'query':{"match_all":{}}})
 
     @classmethod
     def getAllbyIndex(cls, index=None):
         """
         """
-        _all = self._conn.search(index=index, body={"query": {"match_all": {}}})
+        _all = cls._conn.search(index=index, body={"query": {"match_all": {}}})
         _total=_all['hits']['total']
         _count=0
         while True:
             for doc in _all['hits']['hits']:yield doc
             if _count >=_total:break
-            _all = self._conn.search(index=index,
+            _all = cls._conn.search(index=index,
                                      body={"query": {"match_all": {}}},
                                      from_=_count+1)
 
