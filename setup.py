64620f80e164a108b6fd84b7861bd3a2005af867
VulcanoAhab <rodrigoarrigoni@gmail.com>
2017-10-30 22:23:14 -0200

    Starting as module

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="custodi",
+    version='0.1.0',
+    author="VulcanoAhab",
+    packages=["custodi"],
+    url="https://github.com/VulcanoAhab/custodi.git",
+    description="Simple high level API's for Elastic Search, S3 and others",
+    install_requires=[
+
+        "boto3==1.4.7",
+        "elasticsearch==5.4.0",
+        ]
+)
